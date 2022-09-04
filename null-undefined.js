70e0917fb73666aec8d88d21df326a3260e37968
omar-faruuk <omarfaruq.anon@gmail.com>
2022-09-04 13:10:35 +0600

    undefined ex:

---
+++ null-undefined.js
@@ -0,0 +1,23 @@
+// const add =(num1, num2)=>{
+//     console.log(num1 + num2)
+// }
+
+// const result = add(3,2)
+// console.log(result);
+
+
+const add =(num1, num2)=>{
+    // undefined
+    console.log(num2)
+    return;
+}
+
+const result = add(3)
+// undefined
+console.log(result);
+
+
+
+const person = {name:'omar',phone:01225555,}
+//undefined
+console.log(person.age)
